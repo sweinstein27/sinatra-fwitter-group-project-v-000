41bfeabd91ab076dd40940e345478c525b5796ab
Scott Weinstein <sweinstein27@gmail.com>
2018-08-14 15:14:42 +0000

    automatically backed up by learn

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,6 @@
 require 'pry'
 
 class UsersController < ApplicationController
-  get '/users/:slug' do
-    @user = User.find_by_slug(params[:slug])
-    erb :"users/show"
-  end
 
    get '/signup' do
     if !logged_in?
@@ -51,4 +47,22 @@ class UsersController < ApplicationController
     end
   end
 
+  get '/users/:slug' do
+    @user = User.find_by_slug(params[:slug])
+    erb :"users/show"
+  end
+  
  end
+
+
+
+
+
+
+
+
+
+
+
+
+
